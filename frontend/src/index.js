2cdb500143430b8238350333e70d3b048424ab04
Parnika Reddy Nallamilli <nparnikareddy2020@gmail.com>
2026-03-11 23:23:01 +0530

    Add files via upload

---
+++ frontend/src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Navbar from './landing_page/Navbar';
+import Footer from './landing_page/Footer';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import './index.css';
+import HomePage from './landing_page/home/HomePage';
+
+import AboutPage from "./landing_page/about/AboutPage";
+import PricingPage from "./landing_page/pricing/PricingPage";
+import ProductPage from "./landing_page/products/ProductPage";
+import SupportPage from "./landing_page/support/SupportPage";
+import NotFound from './landing_page/NotFound';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  <Navbar/>
+  <Routes>
+    <Route path="/" element={<HomePage/>}/>
+    
+    <Route path="/about" element={<AboutPage/>}/>
+    <Route path="/pricing" element={<PricingPage/>}/>
+    <Route path="/product" element={<ProductPage/>}/>
+    <Route path="/support" element={<SupportPage/>}/>
+    <Route path="*" element={<NotFound/>}/>
+  </Routes>
+  <Footer/>
+  </BrowserRouter>
+  
+);
+
+
